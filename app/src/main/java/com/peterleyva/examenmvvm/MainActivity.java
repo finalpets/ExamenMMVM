ee21451b3ba698554c76af19c8520713c72e7a5c
Peter Leyva <peter.leyva.bazan@gmail.com>
2018-12-12 00:37:48 -0800

    Test: Validating Login User

--- app/src/main/java/com/peterleyva/examenmvvm/MainActivity.java
+++ app/src/main/java/com/peterleyva/examenmvvm/MainActivity.java
@@ -9,8 +9,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.peterleyva.examenmvvm.activities.UserRegisterActivity;
@@ -23,19 +25,48 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public final static int ADD_USER_REQUEST = 1;
+    private static final String TAG = "MainActivity";
     private UserViewModel userViewModel;
+    private EditText edittext_main_email;
+    private EditText edittext_main_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button_main_register);
+        Button button_login = findViewById(R.id.button_main_login);
+
+        edittext_main_email = findViewById(R.id.edittext_main_email);
+        edittext_main_password = findViewById(R.id.edittext_main_password);
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (int x = 0;x<userViewModel.getAllUsers().getValue().size();x++)
+                {
+                    if(userViewModel.getAllUsers().getValue().get(x).getEmail().compareToIgnoreCase(edittext_main_email.getText().toString()) == 0){
+                        if(userViewModel.getAllUsers().getValue().get(x).getPassword().compareToIgnoreCase(edittext_main_password.getText().toString()) == 0) {
+                            Toast.makeText(MainActivity.this, "Password correct", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                            Toast.makeText(MainActivity.this, "Password Incorrect", Toast.LENGTH_SHORT).show();
+                        
+                    }
+                    else
+                        Toast.makeText(MainActivity.this, "Email Incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this,UserRegisterActivity.class);
+
+
                 startActivityForResult(intent,ADD_USER_REQUEST);
             }
         });
@@ -48,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+
+
+
         userViewModel.getAllUsers().observe(this, new Observer<List<User>>() {
             @Override
             public void onChanged(List<User> users) {
