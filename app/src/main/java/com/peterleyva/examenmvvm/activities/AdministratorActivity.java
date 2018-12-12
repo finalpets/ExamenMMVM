72a976031324489a75c521efb56668b4027f91c1
Peter Leyva <peter.leyva.bazan@gmail.com>
2018-12-12 15:36:38 -0800

    Implement Navigation Drawer With all menus

--- app/src/main/java/com/peterleyva/examenmvvm/activities/AdministratorActivity.java
+++ app/src/main/java/com/peterleyva/examenmvvm/activities/AdministratorActivity.java
@@ -1,13 +1,21 @@
 package com.peterleyva.examenmvvm.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.peterleyva.examenmvvm.R;
 import com.peterleyva.examenmvvm.adapters.SucursalAdapter;
 import com.peterleyva.examenmvvm.model.Sucursal;
@@ -15,13 +23,31 @@ import com.peterleyva.examenmvvm.viewmodel.SucursalViewModel;
 
 import java.util.List;
 
-public class AdministratorActivity extends AppCompatActivity {
+public class AdministratorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
     private SucursalViewModel sucursalViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("Administrator");
+
+        drawer = findViewById(R.id.drawer_administrator);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview_administrator);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -40,5 +66,34 @@ public class AdministratorActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_register_sucursal:
+                break;
+            case R.id.nav_register_empleado:
+                break;
+            case R.id.nav_logout:
+                finish();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        super.onBackPressed();
     }
 }
