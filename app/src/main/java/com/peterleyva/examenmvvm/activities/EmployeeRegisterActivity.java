393566ff2eb3e0b7e319162ae6eaf3e718527d6c
Peter Leyva <peter.leyva.bazan@gmail.com>
2018-12-15 22:09:35 -0800

    Adding Spinner and fill all sucursales

--- app/src/main/java/com/peterleyva/examenmvvm/activities/EmployeeRegisterActivity.java
+++ app/src/main/java/com/peterleyva/examenmvvm/activities/EmployeeRegisterActivity.java
@@ -1,13 +1,33 @@
 package com.peterleyva.examenmvvm.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.peterleyva.examenmvvm.R;
+import com.peterleyva.examenmvvm.model.Sucursal;
+import com.peterleyva.examenmvvm.viewmodel.SucursalViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeRegisterActivity extends AppCompatActivity {
+
+    EditText edittext_employeeRegister_name;
+    EditText edittext_employeeRegister_rfc;
+    EditText edittext_employeeRegister_puesto;
+    AppCompatSpinner spinner;
+
+    private SucursalViewModel sucursalViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +38,42 @@ public class EmployeeRegisterActivity extends AppCompatActivity {
         toolbar.setTitle("New Employee");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        edittext_employeeRegister_name = findViewById(R.id.edittext_employeeRegister_name);
+        edittext_employeeRegister_rfc = findViewById(R.id.edittext_employeeRegister_rfc);
+        edittext_employeeRegister_puesto = findViewById(R.id.edittext_employeeRegister_puesto);
+        spinner =findViewById(R.id.spinner_employee_sucursal);
+
+        Button button_employeeRegister_back = findViewById(R.id.button_employeeRegister_back);
+
+        button_employeeRegister_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        sucursalViewModel = ViewModelProviders.of(this).get(SucursalViewModel.class);
+
+        sucursalViewModel.getAllSucursales().observe(this, new Observer<List<Sucursal>>() {
+            @Override
+            public void onChanged(List<Sucursal> sucursals) {
+
+                ArrayList<String> items = new ArrayList<>();
+
+                for (int x= 0;x<sucursals.size();x++)
+                    items.add(sucursals.get(x).getName());
+                ArrayAdapter<String > adapter = new ArrayAdapter<String >(EmployeeRegisterActivity.this, android.R.layout.simple_spinner_item,items);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                spinner.setAdapter(adapter);
+                //spinner_promote_tool_current.setSelection(currentLevel);
+
+                //adapter.setSucursals(sucursals);
+
+
+
+            }
+        });
     }
 }
